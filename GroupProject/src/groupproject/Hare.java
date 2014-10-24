687cd58def2ee80d26e3caced4be20cd45a8135e
Sarah Beggs <s1023011@cplab006.ph.ed.ac.uk>
2014-10-24 23:58:39 +0100

    Hare class

---
+++ GroupProject/src/groupproject/Hare.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groupproject;
+
+/**
+ *
+ * @author s1023011
+ */
+public class Hare extends Animal {
+    
+   //Creates a default Hare object with the initial conditions as in the problem sheet
+    public Hare(){
+        super("prey", 0.2, 0.08);
+    }
+        
+        
+        
+    
+}
